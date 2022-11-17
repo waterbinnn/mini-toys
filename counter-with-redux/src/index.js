7d899b1261afc97d1a8600dcaacb7ef3c9bb4254
waterbinnn <ewaterbin@naver.com>
2022-11-17 14:35:49 +0900

    🎨 Counter 컴포넌트 작성

--- counter-with-redux/src/index.js
+++ counter-with-redux/src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
+
+import { GlobalStyle } from './styles/Global.style';
+
+const initialState = {
+  count: 0,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.num };
+    case 'DECREASE':
+      return { count: state.count - action.num };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <GlobalStyle />
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
