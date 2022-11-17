714a90f0e6f87c8f5270282ca40380ba00acbf6e
waterbinnn <ewaterbin@naver.com>
2022-11-17 19:09:58 +0900

    🔨 Detail 로 Link 연결되게 작성

--- mini-market/src/containers/ProductComponent.jsx
+++ mini-market/src/containers/ProductComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export const ProductComponent = () => {
@@ -9,18 +10,20 @@ export const ProductComponent = () => {
 
     return (
       <div className="column" key={id} style={styles.container}>
-        <div className="ui link cards">
-          <div className="card">
-            <div className="image">
-              <img src={image} alt={title} style={styles.image} />
-            </div>
-            <div className="content">
-              <div className="header">{title}</div>
-              <div className="meta price">$ {price}</div>
-              <div className="meta">{category}</div>
+        <Link to={`product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} style={styles.image} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">$ {price}</div>
+                <div className="meta">{category}</div>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     );
   });
