7f418d623f9f96d19a03cf6a481159ce80b0d635
waterbinnn <ewaterbin@naver.com>
2022-11-17 17:45:30 +0900

    🔨 Product Reducer 생성, 테스트 코드 작성

---
+++ mini-market/src/redux/reducers/productReducer.js
@@ -0,0 +1,16 @@
+import { ActionTypes } from '../constants/action-types';
+
+const initialState = {
+  products: [{ id: 1, title: 'susu', category: 'programmer' }],
+};
+
+export const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+
+    default:
+      return state;
+      break;
+  }
+};
