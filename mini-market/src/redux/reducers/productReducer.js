21a32fc8a07fa5cbacd8e73866c28dae8cb26a46
waterbinnn <ewaterbin@naver.com>
2022-11-17 19:10:32 +0900

    🔨 Product detail 구현 위한 리듀서 작성

--- mini-market/src/redux/reducers/productReducer.js
+++ mini-market/src/redux/reducers/productReducer.js
@@ -14,3 +14,13 @@ export const productReducer = (state = initialState, { type, payload }) => {
       break;
   }
 };
+
+export const selectedProductReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SELECTED_PRODUCT:
+      return { ...state, ...payload };
+
+    default:
+      return state;
+  }
+};
