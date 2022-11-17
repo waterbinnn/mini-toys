ebeb5266184e102f9db76b062573a5d33c07b3e2
waterbinnn <ewaterbin@naver.com>
2022-11-17 19:24:54 +0900

    ♻️ 타이틀 클릭 시 메인 화면 라우트 적용

--- mini-market/src/containers/Header.jsx
+++ mini-market/src/containers/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="ui fixed menu" style={styles.container}>
-      <div className="ui container center" style={styles.header}>
-        <h2>Mini Market</h2>
-      </div>
+      <Link to={'/'}>
+        <div className="ui container center" style={styles.header}>
+          <h2>Mini Market</h2>
+        </div>
+      </Link>
     </div>
   );
 };
