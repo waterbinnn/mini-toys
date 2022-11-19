5499ec75e497e7ecc689d567cdbc84e6ef53a17f
waterbinnn <ewaterbin@naver.com>
2022-11-19 16:14:01 +0900

    ♻️ 분리한 API  import , middleware 사용하여 비동기 방식으로 리팩토링

--- mini-market/src/redux/actions/productAction.js
+++ mini-market/src/redux/actions/productAction.js
@@ -1,17 +1,14 @@
 import { ActionTypes } from '../constants/action-types';
+import fakeStoreApi from '../../apis/fakeStoreApi';
 
-export const setProducts = (products) => {
-  return {
-    type: ActionTypes.SET_PRODUCTS,
-    payload: products,
-  };
+export const fetchProducts = () => async (dispatch) => {
+  const res = await fakeStoreApi.get('/products');
+  dispatch({ type: ActionTypes.FETCH_PRODUCTS, payload: res.data });
 };
 
-export const selectedProduct = (product) => {
-  return {
-    type: ActionTypes.SELECTED_PRODUCT,
-    payload: product,
-  };
+export const fetchProduct = (id) => async (dispatch) => {
+  const res = await fakeStoreApi.get(`/products/${id}`);
+  dispatch({ type: ActionTypes.SELECTED_PRODUCT, payload: res.data });
 };
 
 export const removeSelectedProduct = () => {
