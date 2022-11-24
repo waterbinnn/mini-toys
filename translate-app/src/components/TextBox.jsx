5090b036c4583fc93b404c73f14ceaa2dee9e884
waterbinnn <ewaterbin@naver.com>
2022-11-24 15:07:11 +0900

    🚧 input 입력할 textbox 생성

---
+++ translate-app/src/components/TextBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SelectDropDown from './SelectDropDown';
+
+const TextBox = ({ style }) => {
+  return (
+    <div className={style}>
+      <SelectDropDown />
+      <textarea
+        placeholder={style === 'input' ? 'Enter Text' : 'Translation'}
+        disabled={style === 'output'}
+      />
+    </div>
+  );
+};
+
+export default TextBox;
