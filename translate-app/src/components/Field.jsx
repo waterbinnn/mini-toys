aff73cc95d54f25a9273a8d27216635588973fe5
waterbinnn <ewaterbin@naver.com>
2022-11-24 15:03:10 +0900

    🔥 Remove files

--- translate-app/src/components/Field.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Field = () => {
-  return <div>Field</div>;
-};
-
-export default Field;
--- translate-app/src/components/Translate.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Translate = () => {
-  return <div>Translate</div>;
-};
-
-export default Translate;
