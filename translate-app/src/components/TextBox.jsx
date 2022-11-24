54a0fa882fee5c700f97e00d87ea27d92ba72c4a
waterbinnn <ewaterbin@naver.com>
2022-11-25 00:09:04 +0900

    🔨 TextBox 기능 구현

--- translate-app/src/components/TextBox.jsx
+++ translate-app/src/components/TextBox.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import SelectDropDown from './SelectDropDown';
 
-const TextBox = ({ selectedLanguage, style, setShowModal }) => {
+const TextBox = ({
+  selectedLanguage,
+  style,
+  setShowModal,
+  setTextToTranslate,
+  textToTranslate,
+  translatedText,
+  setTranslatedText,
+}) => {
+  const handleClick = () => {
+    setTextToTranslate('');
+    setTranslatedText('');
+  };
+
   return (
-    <div className={style}>
-      <SelectDropDown
-        style={style}
-        setShowModal={setShowModal}
-        selectedLanguage={selectedLanguage}
-      />
-      <textarea
-        placeholder={style === 'input' ? 'Enter Text' : 'Translation'}
-        disabled={style === 'output'}
-      />
-    </div>
+    <>
+      <div className={style}>
+        <SelectDropDown
+          style={style}
+          setShowModal={setShowModal}
+          selectedLanguage={selectedLanguage}
+        />
+        <textarea
+          disabled={style === 'output'}
+          className={style}
+          placeholder={style == 'input' ? 'Enter text' : 'Translation'}
+          onChange={(e) => setTextToTranslate(e.target.value)}
+          value={style === 'input' ? textToTranslate : translatedText}
+        />
+        {style === 'input' && (
+          <strong className="delete" onClick={handleClick}>
+            ˟
+          </strong>
+        )}
+      </div>
+    </>
   );
 };
 
